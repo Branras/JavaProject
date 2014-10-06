e7acebfd9a0b2465d04a244b3849747c8a084855
Tim <Tim@Akuma>
2014-10-06 11:24:51 +0200

    rename stupid package

--- src/java/info/toegepaste/www/model/Docent.java
+++ src/java/info/toegepaste/www/model/Docent.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package info.toegepaste.model;
+package info.toegepaste.www.model;
 
 import java.io.Serializable;
 import javax.persistence.Basic;
