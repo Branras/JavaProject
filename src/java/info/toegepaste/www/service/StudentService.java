e42d3122a561f88c621b44ab983ced41c3728fbe
brams <brams@Bram>
2014-11-03 10:10:04 +0100

    much security

---
+++ src/java/info/toegepaste/www/service/StudentService.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.toegepaste.www.service;
+
+import info.toegepaste.www.model.*;
+import java.util.List;
+
+/**
+ *
+ * @author brams
+ */
+public interface StudentService {
+    public List<Student> getAllStudenten();
+}
---
+++ src/java/info/toegepaste/www/service/StudentServiceImpl.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.toegepaste.www.service;
+
+import info.toegepaste.www.model.*;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import static javax.ejb.TransactionAttributeType.REQUIRES_NEW;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author brams
+ */
+@Stateless
+public class StudentServiceImpl implements StudentService {
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Override
+    @TransactionAttribute(REQUIRES_NEW)
+    public List<Student> getAllStudenten() {
+        Query q = em.createQuery("SELECT s FROM Student s ORDER BY s.familienaam ASC, s.naam ASC");
+        return (List<Student>) q.getResultList();          
+    }
+}
