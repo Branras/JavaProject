43ed8772e8b141e319da394f4423c4aaba384e84
brams <brams@Bram>
2014-11-05 11:00:48 +0100

    hmm?

--- src/java/info/toegepaste/www/model/Student.java
+++ src/java/info/toegepaste/www/model/Student.java
@@ -33,8 +33,7 @@ import javax.xml.bind.annotation.XmlTransient;
 @Table(name = "student")
 @XmlRootElement
 @NamedQueries({
-    @NamedQuery(name = "Student.findAll", query = "SELECT s FROM Student s"),
-    @NamedQuery(name = "Student.findAllMetKlas", query = "SELECT s FROM Student s JOIN Klas k ON s.klasid = k.klasid"),
+    @NamedQuery(name = "Student.findAll", query = "SELECT s FROM Student s ORDER BY s.klasidid.naam ASC"),
     @NamedQuery(name = "Student.findByStudentid", query = "SELECT s FROM Student s WHERE s.studentid = :studentid"),
     @NamedQuery(name = "Student.findByFamilienaam", query = "SELECT s FROM Student s WHERE s.familienaam = :familienaam"),
     @NamedQuery(name = "Student.findByVoornaam", query = "SELECT s FROM Student s WHERE s.voornaam = :voornaam"),
