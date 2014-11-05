228d747733bd30e7d02e5fcceed2adc1d18f4896
Mathias <Mathias@Mathias-8>
2014-11-05 19:25:03 +0100

    Aan alle problemen komt een eind tenzij je met java werkt

--- src/java/info/toegepaste/www/controller/ExcelController.java
+++ src/java/info/toegepaste/www/controller/ExcelController.java
@@ -6,11 +6,13 @@
 package info.toegepaste.www.controller;
 
 import info.toegepaste.www.service.*;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Scanner;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.servlet.http.Part;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 /**
  *
@@ -22,11 +24,15 @@ public class ExcelController {
     private ExcelService excelservice;
     public Part file;
     public String fileContent;
+    public String test;
+    public HSSFWorkbook workbook;
     
     public void upload() {
     try {
       fileContent = new Scanner(file.getInputStream())
           .useDelimiter("\\A").next();
+      workbook = new HSSFWorkbook(file.getInputStream());
+      test = "2";
     } catch (IOException e) {
       // Error handling
     }
