eb390930d48735f4501b437db790e598b9ed8823
Ben De Cleyn <Ben.de.cleyn1@hotmail.com>
2014-11-06 13:25:43 +0100

    Pdf werkend krijge

--- src/java/info/toegepaste/www/controller/PdfController.java
+++ src/java/info/toegepaste/www/controller/PdfController.java
@@ -28,7 +28,7 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean(name="pdfController")
 public class PdfController {
     public static final String RESULT
-            = "C://Users/hello.pdf";
+            = "D://Studenten.pdf";
 
     public static void Write() throws DocumentException, IOException {
         
