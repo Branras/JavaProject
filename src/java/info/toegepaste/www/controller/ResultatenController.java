38478a1cf383efd124abf0627add6bd2a44f1258
brams <brams@Bram>
2014-11-07 09:54:48 +0100

    AJAX SHIZZLE

---
+++ src/java/info/toegepaste/www/controller/ResultatenController.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.toegepaste.www.controller;
+
+import info.toegepaste.www.model.*;
+import info.toegepaste.www.service.*;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author brams
+ */
+@ManagedBean(name="resultatenController")
+public class ResultatenController {
+    private List<Klas> klassen;
+    private List<Test> testen;
+    private List<Vak> vakken;
+    private List<Student> studenten;
+    
+    @EJB
+    private ResultatenService resultatenservice;
+    
+    @PostConstruct
+    public void init() {
+        klassen = resultatenservice.getAllKlassen();
+    }
+
+    public List<Klas> getKlassen() {
+        return klassen;
+    }
+
+    public void setKlassen(List<Klas> klassen) {
+        this.klassen = klassen;
+    }
+
+    public List<Test> getTesten() {
+        return testen;
+    }
+
+    public void setTesten(List<Test> testen) {
+        this.testen = testen;
+    }
+
+    public List<Vak> getVakken() {
+        return vakken;
+    }
+
+    public void setVakken(List<Vak> vakken) {
+        this.vakken = vakken;
+    }
+
+    public List<Student> getStudenten() {
+        return studenten;
+    }
+
+    public void setStudenten(List<Student> studenten) {
+        this.studenten = studenten;
+    }
+    
+    
+    
+}
---
+++ src/java/info/toegepaste/www/service/ResultatenServiceImpl.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.toegepaste.www.service;
+
+import info.toegepaste.www.model.*;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import static javax.ejb.TransactionAttributeType.REQUIRES_NEW;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author brams
+ */
+@Stateless
+public class ResultatenServiceImpl implements ResultatenService{
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Override
+    @TransactionAttribute(REQUIRES_NEW)
+    public List<Klas> getAllKlassen() {
+        Query q = em.createNamedQuery("Klas.findAll");
+        return (List<Klas>) q.getResultList();        
+    }
+}
