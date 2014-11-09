540e6fc7d19d718d01db096e7a1d85c58787eeb0
Tim <Tim@Akuma>
2014-11-09 10:41:55 +0100

    this commit will fix everything ... probably

--- src/java/info/toegepaste/www/model/Score.java
+++ src/java/info/toegepaste/www/model/Score.java
@@ -59,6 +59,16 @@ public class Score implements Serializable {
     @ManyToOne(optional = false)
     private Test testid;
 
+    boolean editable;
+    
+    public boolean isEditable(){
+        return editable;
+    }
+    
+    public void setEditable(boolean editable){
+        this.editable = editable;
+    }
+    
     public Score() {
     }
 
